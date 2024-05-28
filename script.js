bdac89d7341d6c7fd326d4ef64212c85b1bf82c7
Harshana Panagoda <w1998772@my.westminster.ac.uk>
2024-05-29 01:18:31 +0530

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = "zCTQ0xpsIPB0zDJe3s8abGhMLLgKJjX8";
+const apiUrl = `https://api.apilayer.com/exchangerates_data/convert?apikey=${apiKey}`;
+
+const fromAmount = document.getElementById("fromAmount");
+const fromCurrency = document.getElementById("fromCurrency");
+const toAmount = document.getElementById("toAmount");
+const toCurrency = document.getElementById("toCurrency");
+
+async function fetchRates() {
+  const fromValue = fromCurrency.value;
+  const toValue = toCurrency.value;
+  const amount = fromAmount.value;
+
+  try {
+    const response = await fetch(
+      `${apiUrl}&from=${fromValue}&to=${toValue}&amount=${amount}`
+    );
+    const data = await response.json();
+    const rate = data.result;
+    toAmount.value = rate.toFixed(2);
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error);
+  }
+}
+
+fromAmount.addEventListener("input", fetchRates);
+fromCurrency.addEventListener("change", fetchRates);
+toCurrency.addEventListener("change", fetchRates);
+
+// Initial fetch to populate the toAmount field
+fetchRates();
